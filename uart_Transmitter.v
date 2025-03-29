0830390bc81c015874a25bbcca8318a17e6160bf
Dimitri Cromarty <dimitricromarty@Dimitris-MacBook-Air.local>
2025-03-29 00:51:51 -0400

    Finished created registers and parameters

--- uart_Transmitter.v
+++ uart_Transmitter.v
@@ -13,6 +13,15 @@ module uart_tx(
     parameter s_TX_DATA_BITS = 3'b010;
     parameter s_TX_STOP_BIT = 3'b011;
     parameter s_CLEAN_UP = 3'b100;
+
+    reg [7:0]   r_Clock_Count = 0;
+    reg [2:0]   r_Bit_Index = 0;
+    reg [7:0]   r_Tx_Data = 0;
+    reg [2:0]   r_SM_Main = 0;
+    reg         r_Tx_Done = 0;
+    reg         r_Tx_Active = 0;
+
+    
     
 
 
