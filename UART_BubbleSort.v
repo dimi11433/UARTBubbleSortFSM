fcff3b8d335e93937605f116701fa153a1c96e15
Dimitri Cromarty <dimitricromarty@Dimitris-MacBook-Air.local>
2025-03-31 21:36:09 -0400

    Started on main module will get back to this

--- UART_BubbleSort.v
+++ UART_BubbleSort.v
@@ -1,9 +1,16 @@
+`include uart_Receiver.v
+`include uart_Transmitter.v
+
+
 
 module UART(
-
-
-
-
-
+    input i_CLOCK,
+    input i_Rx_Serial,
+    output i_Tx_Serial
 );
+    
+    
+
+    
+
 endmodule;
