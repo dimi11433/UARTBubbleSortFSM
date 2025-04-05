b8a53bfe705e8e8be0f4e1933d0bb5a8dc82fabb
dimi11433 <164883456+dimi11433@users.noreply.github.com>
2025-04-05 15:39:04 -0400

    Update uart_Receiver.v

--- uart_Receiver.v
+++ uart_Receiver.v
@@ -1,15 +1,15 @@
 module uart_rx(
-    #(parameter CLKS_PER_BIT) //The amount of clicks per bit i guess its given in the name this needs to be set
+    #(parameter CLKS_PER_BIT = 87) //The amount of clicks per bit i guess its given in the name this needs to be set
     input i_clock, //Internal Clock to keep track
     input i_Rx_serial,//Input bit 
     output o_Rx_DV,//Output bit that says whether the data is valid to be recieved or not
     output [7:0] o_Rx_byte,//Output the byte 
 );
-    parameter s_IDLE = 3'b000; // Idle state 
-    parameter s_RX_START_BIT = 3'b001; //Start bit state
-    parameter s_RX_DATA_BITS = 3'b010; //Data bit state
-    parameter s_RX_STOP_BIT = 3'b011; //Stop bit state
-    parameter s_CLEAN_UP = 3'b100; //Clean up bit state 
+    localparam s_IDLE = 3'b000; // Idle state 
+    localparam s_RX_START_BIT = 3'b001; //Start bit state
+    localparam s_RX_DATA_BITS = 3'b010; //Data bit state
+    localparam s_RX_STOP_BIT = 3'b011; //Stop bit state
+    localparam s_CLEAN_UP = 3'b100; //Clean up bit state 
 
     reg r_Rx_DATA_p = 1'b1; //internal register to get value being inputed 
     reg r_Rx_Data = 1'b1; //internal register that saves that value 
@@ -30,21 +30,21 @@ module uart_rx(
     //FSM logic 
     always@(posedge i_clock)begin
         case(r_SM_Main)
-            S_IDLE: 
+            s_IDLE: 
                 begin
                     r_Rx_DV <= 1'b0;
                     r_Clock_Count <= 0;
                     r_Bit_Index <= 0;
-                end 
-                if(i_Rx_Data == 1'b0)begin
+                
+                if(r_Rx_Data == 1'b0)
                     r_SM_Main <= s_RX_START_BIT;
-                end else begin 
+                else  
                     r_SM_Main <= s_IDLE;
                 end 
             s_RX_START_BIT:
                 begin
                     if(r_Clock_Count == (CLKS_PER_BIT - 1)/2)begin
-                        if(i_Rx_Data == 1'b0)begin
+                        if(r_Rx_Data == 1'b0)begin
                             r_Clock_Count <= 0;
                             r_SM_Main <= s_RX_DATA_BITS;
                         end 
@@ -64,7 +64,7 @@ module uart_rx(
                     end 
                     else begin 
                         r_Clock_Count <= 0; //Because its the clock count per bit 
-                        r_Rx_Byte[r_Bit_Index] <= i_Rx_Data; //If that cycle is done save the bit to the index in the byte listing
+                        r_Rx_Byte[r_Bit_Index] <= r_Rx_Data; //If that cycle is done save the bit to the index in the byte listing
                         
                         if(r_Bit_Index < 7)begin
                             r_Bit_Index <= r_Bit_Index +1;
